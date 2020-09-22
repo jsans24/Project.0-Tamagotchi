1893b747a1fd3147ef95329696d38ac09f12819a
jsans24 <jsans24@gmail.com>
2020-09-22 16:40:29 -0700

    Commit 10 - Gave the right arrow functionality

--- js/app.js
+++ js/app.js
@@ -3,5 +3,9 @@ const $rightButton = $('.right')
 const $interactButton = $('.interact')
 
 $leftButton.on('click', function() {
-    $('.rooms').css('transform', 'translate(100%, 0)')
+    $('.rooms').css('transform', 'translate(100%, 0)').css('transition', 'transform 1s linear')
+})
+
+$rightButton.on('click', function() {
+    $('.rooms').css('transform', 'translate(-100%, 0)').css('transition', 'transform 1s linear')
 })
