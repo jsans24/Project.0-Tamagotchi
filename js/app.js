768e7d7b056c22469e68846c4e26adf09e5f7304
jsans24 <jsans24@gmail.com>
2020-09-22 16:22:18 -0700

    Commit 8 - Gave the left arrow functionality

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
 const $leftButton = $('.left')
 const $rightButton = $('.right')
+const $interactButton = $('.interact')
+
+$leftButton.on('click', function() {
+    $('.rooms').css('transform', 'translate(100%, 0)')
+})
