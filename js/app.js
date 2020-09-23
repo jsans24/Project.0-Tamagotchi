97743a20d8d5a09b1b0a6b8045456ce01ad126ac
jsans24 <jsans24@gmail.com>
2020-09-23 10:53:52 -0700

    Commit 13 - Added start menu

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ const $rightButton = $('.right')
 const $interactButton = $('.interact')
 const $centerButton = $('.center')
 
+console.log(`${$('.window').css('width')}`);
+
+$('.start').css('width', `${$('.window').css('width')}`).css('height', `${$('.window').css('height')}`)
+
 $leftButton.on('click', function() {
     $('.rooms').css('transform', `translate(100%, 0)`).css('transition', 'transform 3s linear')
 })
