09fe667a1321174ba68f9574f8b80b85dce0a013
jsans24 <jsans24@gmail.com>
2020-09-23 10:31:29 -0700

    Commit 12 - Added a center button and gave it functionality

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 const $leftButton = $('.left')
 const $rightButton = $('.right')
 const $interactButton = $('.interact')
+const $centerButton = $('.center')
 
 $leftButton.on('click', function() {
-    $('.rooms').css('transform', 'translate(100%, 0)').css('transition', 'transform 1s linear')
+    $('.rooms').css('transform', `translate(100%, 0)`).css('transition', 'transform 3s linear')
 })
 
 $rightButton.on('click', function() {
-    $('.rooms').css('transform', 'translate(-100%, 0)').css('transition', 'transform 1s linear')
+    $('.rooms').css('transform', `translate(-100%, 0)`).css('transition', 'transform 3s linear')
 })
+
+$centerButton.on('click', function() {
+    $('.rooms').css('transform', `translate(0, 0)`).css('transition', 'transform 3s linear')
+})
+
