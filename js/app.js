11f3c27441b4e46e9cb547573967cc75df2d0cea
jsans24 <jsans24@gmail.com>
2020-09-24 12:11:15 -0700

    Commit 24 - added first evolution

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
-const $leftButton = $('.left')
-const $rightButton = $('.right')
-const $interactButton = $('.interact')
-const $centerButton = $('.center')
-const evolution = [{0: $(".training"), 1: $('.rookie'), 2: $('.champion')}]
+const $leftButton = $('.left');
+const $rightButton = $('.right');
+const $interactButton = $('.interact');
+const $centerButton = $('.center');
+const evolution = [{0: $(".training"), 1: $('.rookie'), 2: $('.champion')}];
+const rookieAge = findRandomIntFromInt(5, 10)
+const championAge = findRandomIntFromInt(17, 25)
 let time = 0;
-let room = 1
+let room = 1;
+
+console.log(rookieAge);
+console.log(championAge);
 
 $('.start').css('width', `${$('.window').css('width')}`).css('height', `${$('.window').css('height')}`)
 $interactButton.text('Start');
@@ -42,7 +47,7 @@ function startTimer() {
         $('.hunger').attr('max', digimon.maxHunger)
         $('.boredom').attr('max', digimon.maxBoredom)
         $('.sleep').attr('max', digimon.maxSleep)
-        console.log(time);
+        // console.log(time);
         if (time % 5 === 0) {
             let chance = findRandomIntFromInt(1, 7)
             // console.log(chance);
@@ -50,12 +55,20 @@ function startTimer() {
             if (chance === 2 || chance === 4 || chance === 5 || chance === 7) digimon.hunger++;
             if (chance === 3 || chance === 4 || chance === 6 || chance === 7) digimon.sleep++;
         }
+        console.log(digimon.hunger)
+        console.log(digimon.boredom)
+        console.log(digimon.sleep)
         $('.hunger').val(digimon.hunger)
         $('.boredom').val(digimon.boredom)
         $('.sleep').val(digimon.sleep)
         if (time % 20 === 0) {
             digimon.age = time / 20;
             console.log(digimon.age);
+        }
+        if (time / 20 === rookieAge) {
+            $('.training').css('opacity', 0).css('transition', 'opacity 1s linear')
+            $('.rookie').css('opacity', 1);
+            $('.start').fadeOut().fadeIn().fadeOut().fadeIn().fadeOut()
         }
     }, 1000);
 };
@@ -70,9 +83,9 @@ $interactButton.on('click', function() {
         if(room === 1) $interactButton.text('Play');
         console.log(($('.start').css('display')));
     } else if ($('.start').css('display') == 'none') {
-        if (room === 0) digimon.sleep--;
-        if (room === 1) digimon.boredom--;
-        if (room === 2) digimon.hunger--;
+        if (room === 0 && digimon.sleep > 0) digimon.sleep--;
+        if (room === 1 && digimon.boredom > 0) digimon.boredom--;
+        if (room === 2 && digimon.hunger > 0) digimon.hunger--;
     }
 })
 
