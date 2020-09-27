fab109b16052f28f9d14ca7633d4e3b34a0fdbfb
jsans24 <jsans24@gmail.com>
2020-09-27 16:09:47 -0700

    Commit 33 - cleaned up code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// ---------- Global Variables ---------- //
 const $leftButton = $('.left');
 const $rightButton = $('.right');
 const $interactButton = $('.interact');
@@ -17,25 +18,25 @@ let time = 0;
 let room = 1;
 const $audioEl = $('.audio');
 
+// Sets audio volume
 $audioEl.prop('volume', .15);
 
+// Allows name input
 function updateLetter() {$('.alphabet').text(`${letter[q]}`)};
 $('#name').text(name)
 
 updateLetter()
 
-console.log(rookieAge);
-console.log(championAge);
-
+// controls start menu background size
 $('.start').css('width', `${$('.window').css('width')}`).css('height', `${$('.window').css('height')}`)
 $interactButton.text('Start');
 
-
-
+// just a function to find random numbers
 function findRandomIntFromInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+// tamagotchi class to control the pets stats
 class Tamagotchi{
     constructor() {
         this.maxHunger = 10;
@@ -47,6 +48,7 @@ class Tamagotchi{
         this.age = 0;
     };
     
+    // when pet evolves max stats go up by a random number
     evolve() {
         this.maxHunger += findRandomIntFromInt(0, 3);
         this.maxSleep += findRandomIntFromInt(0, 3);
@@ -54,20 +56,24 @@ class Tamagotchi{
     };
 };
 
+// The pet
 const digimon = new Tamagotchi();
 
 
-
+// the game clock and everything it controlls
 function startTimer() {
     const timer = setInterval(function() {
         time ++;
 
+        // automates the "lights" turning back on
         if ($('.bedroom').css('opacity') == .5) $('.bedroom').css('opacity', 1).css('transition', 'opacity 1s linear');
         
+        // gives the progress bars the same max value as the pet
         $('.hunger').attr('max', digimon.maxHunger)
         $('.boredom').attr('max', digimon.maxBoredom)
         $('.sleep').attr('max', digimon.maxSleep)
         
+        // allows for a random chance of the pets needs increasing every five seconds
         if (time % 5 === 0) {
             let chance = findRandomIntFromInt(1, 7)
             if (digimon.boredom < digimon.maxBoredom) if (chance === 1 || chance === 5 || chance === 6 || chance === 7) digimon.boredom++;
@@ -75,13 +81,16 @@ function startTimer() {
             if (digimon.sleep < digimon.maxSleep) if (chance === 3 || chance === 4 || chance === 6 || chance === 7) digimon.sleep++;
         }
 
+        // gives the progress bars the same value as the pet
         $('.hunger').val(digimon.hunger)
         $('.boredom').val(digimon.boredom)
         $('.sleep').val(digimon.sleep)
         
+        // allows the pet to age every 20 seconds
         digimon.age = Math.floor(time / 20);
         $('#age').text(digimon.age)
         
+        // allows the pets first evolution at a randomly chosen age
         if (time / 20 === rookieAge) {
             $trainingEl.css('opacity', 0).css('transition', 'opacity 1s linear')
             $rookieEl.css('opacity', 1);
@@ -89,6 +98,7 @@ function startTimer() {
             digimon.evolve()
         }
         
+        // allows the pets second evolution at a randomly chosen age
         if (time / 20 === championAge) {
             $rookieEl.css('opacity', 0).css('transition', 'opacity 1s linear')
             $championEl.css('opacity', 1);
@@ -96,10 +106,14 @@ function startTimer() {
             digimon.evolve()
         }
         
+        // when any stat reaches max brings up the gameover screen
         if (digimon.hunger >= digimon.maxHunger || digimon.sleep >= digimon.maxSleep || digimon.boredom >= digimon.maxBoredom) {
             $audioEl.attr('src', './assets/digimon world 3 - game over copy.m4a')
+            // ends clock
             clearInterval(timer)
             $interactButton.text('Retry');
+
+            // controls which pet model appears on the game over screen
             if($eggEl.attr('src') == './assets/egg1.png') {
                 if ($trainingEl.css('opacity') == 1) $('.dead').attr('src', './assets/dead_tokomon.png');
                 if ($rookieEl.css('opacity') == 1) $('.dead').attr('src', './assets/dead_patamon.png');
@@ -115,6 +129,7 @@ function startTimer() {
                 if ($rookieEl.css('opacity') == 1) $('.dead').attr('src', './assets/dead_gabumon.png');
                 if ($championEl.css('opacity') == 1) $('.dead').attr('src', './assets/dead_garurumon.png');
             }
+
             $('.dead').css('opacity', 1)
             $trainingEl.css('visibility', 'hidden')
             $rookieEl.css('visibility', 'hidden')
@@ -125,7 +140,9 @@ function startTimer() {
     }, 1000);
 };
 
+// ---------- Event Listeners ---------- //
 
+// gives the interact button functionality
 $interactButton.on('click', function() {
     if ($eggEl.css('opacity') == 1) {
         $('.alphabet').css('visibility', 'visible')
@@ -161,7 +178,7 @@ $interactButton.on('click', function() {
 })
 
 
-
+// gives the left arrow button functionality
 $leftButton.on('click', function() {
     if ($eggEl.css('opacity') == 1) {
         $eggEl.attr('src', './assets/egg1.png')
@@ -181,6 +198,7 @@ $leftButton.on('click', function() {
     }
 })
 
+// // gives the down arrow button functionality
 $centerButton.on('click', function() {
     if ($eggEl.css('opacity') == 1) {
         $eggEl.attr('src', './assets/egg.png')
@@ -202,6 +220,7 @@ $centerButton.on('click', function() {
     }
 })
 
+// // gives the right arrow button functionality
 $rightButton.on('click', function() {
     if ($eggEl.css('opacity') == 1) {
         $eggEl.attr('src', './assets/egg2.png')
